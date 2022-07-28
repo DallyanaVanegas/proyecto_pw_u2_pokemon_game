b6d64582a083c263bbbefb5487b10f13b4ce7771
Dallyana Vanegas <davanegas@uce.edu.ec>
2022-07-27 19:54:15 -0500

    Taller19

--- src/components/PokemonPage.vue
+++ src/components/PokemonPage.vue
@@ -1,17 +1,35 @@
 <template>
   <PokemonPicture :pokemonId = "16" :mostrarPokemon = "false"/>
-  <PokemonOptions />
+  <PokemonOptions :pokemon = "pokemonArr" />
 </template>
 
 <script>
 import PokemonPicture from "@/components/PokemonPicture";
 import PokemonOptions from "@/components/PokemonOptions";
+import getPokemonOptions from "../helpers/obtenerOpcionesPokemon"
+
+//getPokemonOptions()
 
 export default {
   components: {
     PokemonPicture,
     PokemonOptions,
   },
+  data(){
+    return {
+      pokemonArr:[]
+    }
+  },
+  methods:{
+    async obtenerPokemonsArray(){
+      this.pokemonArr = await getPokemonOptions()
+      console.log("impresion desde el page")
+      console.log(this.pokemonArr)
+    }
+  },
+  mounted(){
+    this.obtenerPokemonsArray()
+  }
 };
 </script>
 
